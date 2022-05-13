c97c42f7563adffc52edf975bb981b42afd69f5a
Stefan Ingthorsson <ingthorsson@gmail.com>
2022-05-13 23:27:45 +0000

    Add Giphy code

--- frontend/src/views/Giphy.js
+++ frontend/src/views/Giphy.js
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+function useGiphy(query) {
+
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=5tRKFNX8sHZZdDLQT3ml7g0B7lhGqPej&q=${query}&limit=5&offset=0&rating=g&lang=en`);
+        const json = await response.json();
+
+        console.log(json.data)
+        setResults(
+          json.data.map((item) => {
+            return item.images.preview.mp4;
+          })
+        )
+
+      } finally {
+        setLoading(false);
+      }
+    }
+    if (query !== '') {
+      fetchData();
+    }
+  }, [query]);
+
+  return [results, loading];
+}
 
 function Giphy() {
+
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+  const [results, loading] = useGiphy(query);
+
   return (
-      <div className='App'>
-          Giphy
+    <div className="app">
+      <h1>Async React Hooks</h1>
+
+      <form onSubmit={e => {
+        e.preventDefault();
+        setQuery(search);
+      }}>
+        <input
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          placeholder="Search for Gifs!"
+        />
+        <button type="submit" >Search</button>
+      </form>
+
+      <div className="gifs">
+        {loading ? <h1>GIMMY GIF'S</h1> :
+          results.map((item) => (
+            <video key={item} autoPlay loop src={item} />
+          ))}
       </div>
 
+    </div>
   );
 }
 
